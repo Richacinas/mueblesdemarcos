389106ae7e5ab4be035aba88126db12111f496e5
Ricardo Martínez <rmartrenado@gmail.com>
2015-06-24 01:18:11 +0200

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+/**
+ * Main APP js
+ * By: Ricardo Martínez 
+ *
+ */
+
+
+$(document).ready(function() {
+
+    $('#contact').html5form({
+            allBrowsers : true,
+            responseDiv : '#response',
+            messages: 'es',
+            colorOn :'#fff'
+    });
+    $('#spam-check').removeAttr('required');
+
+});
